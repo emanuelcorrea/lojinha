10136639c8d92f50e4b19513655e62b1c373faed
Emanuel Corrêa <emanuel.silvati@gmail.com>
2019-12-23 00:25:12 -0300

    Footer started

---
+++ App/Views/templates/footer.php
@@ -0,0 +1,42 @@
+<footer class="footer-home">
+    <section class="social">
+        <div class="news-letter">
+            <div class="container">
+                <div class="logo">
+                    <img src="<?php echo DIRIMG; ?>home/10off.png" alt="">
+                </div>
+                <form action="get">
+                    <input type="text" name="nome" id="nome" placeholder="Nome:">
+                    <input type="text" name="nome" id="email" placeholder="Email:">
+                    <input type="submit" value="CADASTRAR" name="submit">
+                </form>
+            </div>
+        </div>
+        <div class="social-medias">
+            <div class="container">
+                <div class="social-title">
+                    <h4>SIGA NOSSAS REDES SOCIAIS: </h4>
+                </div>
+                <div class="social-icons">
+                    <nav>
+                        <ul>
+                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                            <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                            <li><a href="#"><i class="fab fa-spotify"></i></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="business">
+        <nav>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </section>
+</footer>
